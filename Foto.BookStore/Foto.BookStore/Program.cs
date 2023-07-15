95099ee1e1ee754520260f0de75a8a251472d047
AJAY SINGH YADAV <rawdy2012@gmail.com>
2023-07-15 20:04:45 +0530

    Created Book Store Application

--- Foto.BookStore/Foto.BookStore/Program.cs
+++ Foto.BookStore/Foto.BookStore/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//app.MapGet("/", () => "Hello World!");
+
+IWebHostEnvironment env = builder.Environment;
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+    //endpoints.MapGet("/", async context =>
+    //{
+    //    if (env.IsProduction())
+    //    {
+    //        await context.Response.WriteAsync("Hello from dev!");
+    //    }
+        
+    //});
+   
+});
 
 app.Run();
