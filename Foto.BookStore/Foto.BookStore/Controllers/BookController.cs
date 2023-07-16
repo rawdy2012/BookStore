8185edde47b4e7a66a24b95516ae31dda203ddea
AJAY SINGH YADAV <rawdy2012@gmail.com>
2023-07-16 18:18:00 +0530

    Added Book Model & Book Repository

---
+++ Foto.BookStore/Foto.BookStore/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using Foto.BookStore.Models;
+using Foto.BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foto.BookStore.Controllers
+{
+   
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository=null;
+        public BookController()
+        {
+            _bookRepository= new BookRepository();  
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();  
+        }
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+        public List<BookModel> SearchBooks(string bookName,string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName); /*$"Book with name = {bookName} & author = {authorName}";*/
+        }
+    }
+}
---
+++ Foto.BookStore/Foto.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,31 @@
+﻿using Foto.BookStore.Models;
+
+namespace Foto.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById(int id) 
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public  List<BookModel> SearchBook(string title,string authorName) 
+        {
+        return DataSource().Where(x=>x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+        }
+        private List<BookModel> DataSource() 
+        { 
+         return new List<BookModel> ()
+         { 
+             new BookModel(){Id=1,Title="MVC",Author="Ajay"},
+             new BookModel(){Id=2,Title="Sql",Author="Ajay"},
+             new BookModel(){Id=3,Title="Bootstrap",Author="Param"},
+             new BookModel(){Id=4,Title="HTML",Author="Vinay"},
+             new BookModel(){Id=5,Title="Jquery",Author="Param"},
+         };    
+        }
+    }
+}
